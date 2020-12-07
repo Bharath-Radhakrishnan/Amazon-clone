1084a209f1b0677990aa20a096175689371c8faf
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-07 18:17:26 +0530

    import header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello World!!!djshdjhdsjdshjdsgjdgsdgdghdsghgghghg</h1>
     </div>
   );

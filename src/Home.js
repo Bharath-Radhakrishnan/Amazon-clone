0ca3fd66d11e6c323a2ec48c0a991557b1a43438
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-08 13:22:14 +0530

    add Elements

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,31 @@
 import React from "react";
+import "./home.css";
+import Product from "./Product";
 
 function Home() {
   return (
-    <div>
-      <h1>This is the Home</h1>
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/Cnnjpp/OnePlus/OnePlus_BAU/4thdec/D18734439_BAU_OnePlus_8T_Tall_hero_1500x600._CB414727933_.jpg"
+          alt="sorry.error"
+        />
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+
+          <Product />
+        </div>
+
+        <div className="home__row">
+          <Product />
+        </div>
+      </div>
     </div>
   );
 }

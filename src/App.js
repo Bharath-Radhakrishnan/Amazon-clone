3b5291f6978e18eecf7f05ff24d84ef47f5860f9
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-14 13:13:00 +0530

    add checkout compo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           </Route>
           <Route path="/checkout">
             <Header />
-            <h1>checkoooooooooouuuut</h1>
+            <Checkout />
           </Route>
         </Switch>
       </div>
---
+++ src/Checkout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./checkout.css";
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://m.media-amazon.com/images/S/aplus-media/sc/639bbef0-9746-4f6e-871d-e83914870f24.__CR1,0,2021,625_PT0_SX970_V1___.jpg"
+        />
+        <div>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
+        </div>
+      </div>
+      <div className="chekout__right">
+        <h2>The Subtotal will go here</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;

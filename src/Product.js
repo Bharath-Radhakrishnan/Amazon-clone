4a3beac7a2868d3e11a18f47d93a21aadf0aa186
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-15 01:15:45 +0530

    remove unwanted consolelog

--- src/Product.js
+++ src/Product.js
@@ -4,7 +4,6 @@ import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, price, rating, image }) {
   const [{ basket }, dispatch] = useStateValue();
-  console.log("this is the baskt>>>>", basket);
   const addToBasket = () => {
     dispatch({
       type: "ADD_TO_BASKET",

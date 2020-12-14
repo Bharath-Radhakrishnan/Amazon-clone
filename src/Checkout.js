5524df128ae4a1a1c7947f02e73292b27bbc0e62
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-15 01:17:07 +0530

    add producttile

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,27 @@
 import React from "react";
 import "./checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import Product from "./Product";
+import CheckoutProduct from "./CheckoutProduct";
+
 function Checkout() {
+  const [{ basket, dispatch }] = useStateValue();
+  const itemCount = basket?.length;
+
+  const generateProducts = () => {
+    return basket.map((element) => {
+      return (
+        <CheckoutProduct
+          id={element.id}
+          title={element.title}
+          image={element.image}
+          price={element.price}
+          rating={element.rating}
+        />
+      );
+    });
+  };
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -10,7 +30,10 @@ function Checkout() {
           src="https://m.media-amazon.com/images/S/aplus-media/sc/639bbef0-9746-4f6e-871d-e83914870f24.__CR1,0,2021,625_PT0_SX970_V1___.jpg"
         />
         <div>
-          <h2 className="checkout__title">Your Shopping Basket</h2>
+          <h2 className="checkout__title">
+            Your Shopping Basket contains {itemCount} items
+          </h2>
+          {generateProducts()}
         </div>
       </div>
       <div className="chekout__right">

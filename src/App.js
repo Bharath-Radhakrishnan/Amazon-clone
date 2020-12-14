b8f24eaf50764eff933cb437e78c1e9c77d0eb09
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-14 12:35:11 +0530

    add checkout route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact="true">
+            <Header />
+            <Home />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <h1>checkoooooooooouuuut</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

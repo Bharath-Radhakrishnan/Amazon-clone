0711a3fa87883a63de122abbf5454a6857833871
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-15 01:14:45 +0530

    add remove from basket action

--- src/reducer.js
+++ src/reducer.js
@@ -5,6 +5,11 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_BASKET":
       return { ...state, basket: [...state.basket, action.item] };
+    case "REMOVE_FROM_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket.filter((item) => item.id != action.item.id)],
+      };
     default:
       return state;
   }

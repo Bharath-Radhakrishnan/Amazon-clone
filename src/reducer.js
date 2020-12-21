3113c14bbc03b54053839d4825364ac3ae414365
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-21 13:18:23 +0530

    add user state and SET_USER action

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,6 @@
 export const initialState = {
   basket: [],
+  user: null,
 };
 
 export const getBasketTotal = (basket) =>
@@ -21,6 +22,8 @@ const reducer = (state, action) => {
       return { ...state, basket: newBasket };
     default:
       return state;
+    case "SET_USER":
+      return { ...state, user: action.user };
   }
 };
 

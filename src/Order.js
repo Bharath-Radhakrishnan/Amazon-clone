d3a8affdc0f003faa6c2e81dc456758f4eae20d9
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2020-12-27 23:01:32 +0530

    add currency format

--- src/Order.js
+++ src/Order.js
@@ -1,13 +1,18 @@
 import React from "react";
 import moment from "moment";
 import CheckoutProduct from "./CheckoutProduct";
+import CurrencyFormat from "react-currency-format";
+import "./order.css";
+
 function Order({ order }) {
   console.log(order);
   return (
     <div className="order">
       <h2>order</h2>
       <p>{moment.unix(order.data.created).format("MMMM Do YYYY,h:mma")}</p>
-      <small>{order.id}</small>
+      <p className="order__id ">
+        <small>{order.id}</small>
+      </p>
       {order.data.basket?.map((item) => {
         return (
           <CheckoutProduct
@@ -16,9 +21,22 @@ function Order({ order }) {
             image={item.image}
             rating={item.rating}
             price={item.price}
+            hideButton={true}
           />
         );
       })}
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <h3 className="order__total">Order total:{value}</h3>
+          </>
+        )}
+        value={order.data.amount / 100}
+        decimalScale={2}
+        prefix="$"
+        displayType={"text"}
+        thousandSeparator={true}
+      />
     </div>
   );
 }
